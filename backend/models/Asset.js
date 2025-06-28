b1fe55a1f1f12f553c098d504d3c1712623536c2
Binduvarsha23 <123391226+Binduvarsha23@users.noreply.github.com>
2025-06-28 15:06:20 +0530

    Update Asset.js

--- backend/models/Asset.js
+++ backend/models/Asset.js
@@ -1,6 +1,8 @@
+// src/models/Asset.js
 import mongoose from "mongoose";
 
 const assetSchema = new mongoose.Schema({
+  userId: { type: String, required: true }, // Added userId field
   name: String,
   type: String,
   value: Number,
