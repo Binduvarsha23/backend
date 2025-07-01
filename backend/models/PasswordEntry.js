dd90f5a583c6c0f5e04469017672e6e0d3bf6fc1
Binduvarsha23 <123391226+Binduvarsha23@users.noreply.github.com>
2025-07-01 16:59:37 +0530

    Update PasswordEntry.js

--- backend/models/PasswordEntry.js
+++ backend/models/PasswordEntry.js
@@ -5,6 +5,7 @@ const passwordEntrySchema = new mongoose.Schema({
   blockName: { type: String, required: true }, // category
   data: { type: String, required: true }, // AES Encrypted string
   createdAt: { type: Date, default: Date.now },
+  favorite: { type: Boolean, default: false },
 });
 
 export default mongoose.model('PasswordEntry', passwordEntrySchema);
