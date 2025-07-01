1d6562b02892cd055068715dfe01d477fecb4579
Binduvarsha23 <123391226+Binduvarsha23@users.noreply.github.com>
2025-07-01 13:05:51 +0530

    Update Asset.js

--- backend/models/Asset.js
+++ backend/models/Asset.js
@@ -2,13 +2,14 @@
 import mongoose from "mongoose";
 
 const assetSchema = new mongoose.Schema({
-  userId: { type: String, required: true }, // Added userId field
+  userId: { type: String, required: true },
   name: String,
   type: String,
   value: Number,
   location: String,
   nominee: String,
   description: String,
+  imageUrl: String, // <--- NEW FIELD: Add this line for image URL
 });
 
 export default mongoose.model("Asset", assetSchema);
