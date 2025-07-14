3dd347367ce0c02f2c0acb031712e142f19dac02
Binduvarsha23 <123391226+Binduvarsha23@users.noreply.github.com>
2025-07-14 18:40:33 +0530

    Update adroute.js

--- backend/routes/adroute.js
+++ backend/routes/adroute.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import multer from 'multer';
 import Ad from '../models/ad.js';
-import { checkAccessLevel, requireReadWrite } from './adminaccess.js';
+import { checkAccessLevel, requireReadWrite } from './adminMiddleware.js';
 
 const router = express.Router();
 const storage = multer.memoryStorage();
