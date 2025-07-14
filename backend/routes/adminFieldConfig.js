36f0f833949d528e0ed0aac67fb5b9e0c20d5d34
Binduvarsha23 <123391226+Binduvarsha23@users.noreply.github.com>
2025-07-14 18:32:24 +0530

    Update adminFieldConfig.js

--- backend/routes/adminFieldConfig.js
+++ backend/routes/adminFieldConfig.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import BlockMetadata from '../models/BlockMetadata.js';
 import FieldConfig from '../models/FieldConfig.js';
-import { checkAccessLevel, requireReadWrite } from '../middleware/adminMiddleware.js';
+import { checkAccessLevel, requireReadWrite } from './adminaccess.js';
 
 const router = express.Router();
 
