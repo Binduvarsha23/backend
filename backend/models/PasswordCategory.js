4b5edf20d1c8e0fbb9ea23375ed599b06add4e66
Binduvarsha23 <123391226+Binduvarsha23@users.noreply.github.com>
2025-07-15 19:58:03 +0530

    Update PasswordCategory.js

--- backend/models/PasswordCategory.js
+++ backend/models/PasswordCategory.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 const passwordCategorySchema = new mongoose.Schema({
   userId: { type: String, required: true },
   blockName: { type: String, required: true },
+  summary: { type: String},
   createdAt: { type: Date, default: Date.now },
 });
 
